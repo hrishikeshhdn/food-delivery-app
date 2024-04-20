ebc83ee40531077bfd15b275124a0fa2b5eb086a
Hrishikesh <hrishikeshhdn@gmail.com>
2024-04-20 21:26:56 +0530

    frontend copy

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+      <div className="container-fluid">
+        <Link className="navbar-brand fs-1 fst-italic" to="/">GoFood</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2">
+            <li>
+              <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+            </li>
+            {(localStorage.getItem("authToken")) ?
+
+              <li>
+                <Link className="nav-link active fs-5" aria-current="page" to="/">My Orders</Link>
+              </li>
+              
+              : ""}
+          </ul>
+          {(!localStorage.getItem("authToken")) ?
+            <div className='d-flex'>
+
+              <Link className="btn bg-white text-success mx-1" to="/login" >Login</Link>
+              <Link className="btn bg-white text-success mx-1" to="/creatuser">Signup</Link>
+
+            </div>
+            :
+            <div>
+              <div className='btn bg-white text-success mx-2 '>
+                My Cart
+              </div>
+              <div className='btn bg-white text-success mx-2' >
+                Logout
+              </div>
+            </div>
+          }
+        </div>
+      </div>
+    </nav>
+  )
+}
